abd009998c8b49c2db429522c3db73059d5edbfe
webyak <webyak@outlook.com>
2016-08-19 12:10:50 +0700

    Commit isomorphic Webpack config.

--- tools/isomorphic.prod.config.js
+++ tools/isomorphic.prod.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   assets: {
-    woffFonts: {
-      extensions: ['woff', 'woff2'],
-      regular_expression: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    },
-    fonts: {
-      extensions: ['ttf', 'eot', 'svg'],
-      regular_expression: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    css: {
+      extensions: ['css'],
     },
     images: {
       extensions: ['png', 'jpg', 'gif', 'ico'],
